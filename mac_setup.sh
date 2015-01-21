968da1304fa8687d4580da28afa62b64548ee1f9
Brian Casey Kulm <mlukyesac@gmail.com>
2015-01-20 20:14:45 -0600

    Make the mac setup create symbolic links

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 # symbolic link for .bash_profile
-ln ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 
 # symbolic link for .vimrc
-ln ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
