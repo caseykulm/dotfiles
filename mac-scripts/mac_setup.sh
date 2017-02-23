15fc1bbfb877dd0c4d16acb754ea726264ff7cef
Casey Kulm <mlukyesac@gmail.com>
2017-02-22 21:16:57 -0600

    Add .tmux.conf and update mac setup script

--- mac-scripts/mac_setup.sh
+++ mac-scripts/mac_setup.sh
@@ -1,8 +1,38 @@
 #! /bin/bash
 
 # symbolic link for .zshrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+link_input_file() {
+  INPUT_FILE=$1
+  if [ -f "~/$INPUT_FILE" ]
+  then
+    cp ~/$INPUT_FILE ~/$INPUT_FILE.backup
+    echo "Created a backup at ~/$INPUT_FILE.backup"
+    rm ~/$INPUT_FILE
+  fi
+  ln -s ~/dotfiles/$INPUT_FILE ~/$INPUT_FILE
+}
 
-# symbolic link for .vimrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+handle_file_exists() {
+  INPUT_FILE=$1
+  echo "$INPUT_FILE already exists, do you want to replace it? (y/n)"
+  read answer
+  if [ echo "$answer" | grep -iq "^y" ]
+  then
+    link_input_file $INPUT_FILE
+  fi
+}
+
+setup_file_locally() {
+  INPUT_FILE=$1
+  if [ -f "~/$INPUT_FILE" ]
+  then
+    handle_file_exists $INPUT_FILE
+  else
+    link_input_file $INPUT_FILE
+  fi
+}
+
+setup_file_locally ".zshrc"
+setup_file_locally ".vimrc"
+setup_file_locally ".tmux.conf"
 
