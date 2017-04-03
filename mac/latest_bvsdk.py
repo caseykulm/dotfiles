5c69513b5b5b9d8b05fc46a2ce4c2fe966e72abd
Casey Kulm <casey.kulm@bazaarvoice.com>
2017-04-03 10:21:20 -0500

    Update bvsdk script

--- mac/latest_bvsdk.py
+++ mac/latest_bvsdk.py
@@ -6,7 +6,8 @@ def print_latest_bvsdk():
     r = requests.get("http://search.maven.org/solrsearch/select?q=g:%22com.bazaarvoice.bvandroidsdk%22+AND+a:%22common%22&rows=20&wt=json")
     json_resp = r.json()
     latestVersion = json_resp.get("response").get("docs")[0].get("latestVersion")
-    print latestVersion + " -> http://search.maven.org/#search%7Cga%7C1%7Cg%3A%22com.bazaarvoice.bvandroidsdk%22"
+    print "The latest version that people can see in the mavenCentral browser and Github badges..."
+    print latestVersion + " -> http://search.maven.org/#search%7Cga%7C1%7Cg%3A%22com.bazaarvoice.bvandroidsdk%22\n"
 
 try:
     print_latest_bvsdk()
@@ -14,11 +15,27 @@ except Exception as e:
     print "Failed to fetch/parse latest version"
     print "http://search.maven.org/#search%7cga%7c1%7cg:%22com.bazaarvoice.bvandroidsdk%22%20AND%20a:%22common%22"
 
-print "Unsynced Maven: https://repo1.maven.org/maven2/com/bazaarvoice/bvandroidsdk/common/"
-print "Internal Maven Snapshots (Requires LDAP): https://repo.bazaarvoice.com/nexus/content/groups/bazaarvoice/com/bazaarvoice/bvandroidsdk/common/"
-print "OSS Sonatype Staging (Requires BV Credentials): oss.sonatype.org/#stagingRepositories"
-print "Public Docs: https://bazaarvoice.github.io/bv-android-sdk/"
-print "Private Docs: http://utilities.qa.us-east-1.nexus.bazaarvoice.com/github-pages-android-sdk/"
-print "Public Repo: https://github.com/bazaarvoice/bv-android-sdk/"
-print "Private Repo: https://github.com/bazaarvoice/bv-android-sdk-dev"
+print "The latest version that people can actually pull down from mavenCentral using maven/gradle/etc."
+print "https://repo1.maven.org/maven2/com/bazaarvoice/bvandroidsdk/common/\n"
+
+print "The internal Bazaarvoice Maven Snapshots (Requires LDAP credentials)"
+print "https://repo.bazaarvoice.com/nexus/content/groups/bazaarvoice/com/bazaarvoice/bvandroidsdk/common/\n"
+
+print "The mavenCentral staging console where you can promote a release (Requires BV Open Source credentials)"
+print "oss.sonatype.org/#stagingRepositories\n"
+
+print "The mavenCentral snapshots"
+print "https://oss.sonatype.org/content/repositories/snapshots/com/bazaarvoice/bvandroidsdk/common/\n"
+
+print "Public Github Pages Docs"
+print "https://bazaarvoice.github.io/bv-android-sdk/\n"
+
+print "Private AWS QA Docs"
+print "http://utilities.qa.us-east-1.nexus.bazaarvoice.com/github-pages-android-sdk/\n"
+
+print "Public Github Repo"
+print "https://github.com/bazaarvoice/bv-android-sdk/\n"
+
+print "Private Github Repo"
+print "https://github.com/bazaarvoice/bv-android-sdk-dev\n"
 
